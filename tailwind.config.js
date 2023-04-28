ef2cd90437baeea6d85959a19855fcaf7b291e94
kwtuku <76758720+kwtuku@users.noreply.github.com>
2023-04-28 23:20:25 +0900

    feat: ラジオボタンのスタイルを調整

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: [
+    "./app/components/**/*.html.{erb,haml}",
     "./app/views/**/*.html.{erb,haml}",
     "./app/helpers/**/*.rb",
     "./app/assets/stylesheets/**/*.css",
